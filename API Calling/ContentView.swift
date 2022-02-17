8e0c6c8ed6bde1711c3c136f48e947109a8fd63d
Daisy Guerrero <dguerrero1@bsd220.org>
2022-02-16 21:37:08 -0600

    Successful API Calling

--- API Calling/ContentView.swift
+++ API Calling/ContentView.swift
@@ -13,13 +13,42 @@ struct ContentView: View {
         NavigationView {
             List (games)  { game in
                 NavigationLink(
-                    destination: Text(game.game_url)
-                        .padding(),
+                    destination: VStack {
+                        Text(game.short_description)
+                        Text(game.game_url)
+                        Text(game.genre)
+                        Text(game.platform)
+                        .padding()
+                    },
                     label: {
-                        Text(game.title)
+                            Text(game.title)
                     })
             }
             .navigationTitle("Games")
+        }
+        .onAppear(perform: {
+            getGames()
+        })
+    }
+    func getGames() {
+        let apiKey = "?rapidapi-key=001b5c5e5emsh7fc081c3e3e9f4bp1b46f4jsn80c50e3dfd2c"
+        let query = "https://free-to-play-games-database.p.rapidapi.com/api/games\(apiKey)"
+        if let url = URL(string : query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                let contents = json.arrayValue
+                for item in contents {
+                    let title = item["title"].stringValue
+                    let short_description = item["short_description"].stringValue
+                    let game_url = item["game_url"].stringValue
+                    let genre = item["genre"].stringValue
+                    let platform = item["platform"].stringValue
+                    let game = Game(title: title, short_description: short_description, game_url: game_url, genre: genre, platform: platform)
+                    games.append(game)
+                }
+                return
+            }
+            
         }
     }
 }
@@ -27,9 +56,11 @@ struct ContentView: View {
 struct Game : Identifiable {
     let id = UUID()
     var title : String
+    var short_description : String
     var game_url : String
+    var genre : String
+    var platform : String
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
